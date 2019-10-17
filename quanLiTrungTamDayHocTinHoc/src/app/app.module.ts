e672650ab6bedeca38d1f80f7c73a62103793a5d
zitxiem1998 <zitxiem1998@gmail.com>
2019-10-17 14:32:02 +0700

    tao cau truc thu muc

--- quanLiTrungTamDayHocTinHoc/src/app/app.module.ts
+++ quanLiTrungTamDayHocTinHoc/src/app/app.module.ts
@@ -5,16 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './share/material/material.module';
+import { ListStudentComponent } from './component/student/list-student/list-student.component';
+import { ListTeacherComponent } from './component/teacher/list-teacher/list-teacher.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListStudentComponent,
+    ListTeacherComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
